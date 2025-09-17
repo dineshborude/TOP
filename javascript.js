c35a033d898bb7727db2eaba7c3bb88c1fc92633
dineshborude <borudedinesh2@gmail.com>
2025-09-17 15:45:53 -0400

    slice method

---
+++ javascript.js
@@ -0,0 +1,21 @@
+// console.log('Hello JS');
+
+const pi = 3.14;
+
+
+// console.log(pi); // What will be output?
+
+// console.log((3 + 2) - 76 * (1 + 1));
+
+function capatalize(str) {
+            str = str.toLowerCase();
+            return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+console.log(capatalize("dinesH"));
+
+function lastLetter(str) {
+    return str.slice(-1);
+}
+
+console.log(lastLetter("abcd"))
